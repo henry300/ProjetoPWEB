7dafba9020a0dc3cb28853dc7af29a8d4eb7371e
Claudionor-Cosme <claudionorcosmefilho@gmail.com>
2026-05-20 20:22:08 -0300

    🟢 adicionado a classe vendedor

---
+++ src/models/vendedor.ts
@@ -0,0 +1,16 @@
+export class vendedor{
+    id_vendedor: number
+    nome: string
+    matricula: string
+    comissao_percentual: number
+
+    constructor(id_vendedor: number, nome: string, matricula: string, comissao_percentual: number){
+        this.id_vendedor = this.geraId()
+        this.nome = nome
+        this.matricula = matricula
+        this.comissao_percentual = comissao_percentual
+    }
+    private geraId(): number{
+        return Date.now();
+    }
+}

d71fd615c54f5c3f33179fc912fd7a5759af33c8
BrunoMendesMorais <mendesdemoraisbruno@gmail.com>
2026-05-24 21:21:25 -0300

    🟢 Feat: Criado rotas de produto

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import express, { Request, Response } from "express";
+import { atualizarCarro, cadastrarCarro, deleteCarro, listaCarros, listarCarroPorId, listarCarrosDisponiveis } from "./controllers/carroController";
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 app.use(express.json());
 
 app.listen(PORT, () => console.log(`API rodando na URL : http://localhost:${PORT}`));
+
+app.get("/carros",listaCarros)
+app.get("/carros/:id",listarCarroPorId)
+app.get("/carros/disponiveis",listarCarrosDisponiveis)
+app.post("/carros",cadastrarCarro)
+app.put("/carros/:id",atualizarCarro)
+app.delete("/carros/:id",deleteCarro)
---
+++ src/controllers/carroController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express"
+import { CarroService } from "../services/CarroService"
+const carroService = new CarroService
+
+export function listaCarros(){
+
+}
+export function listarCarroPorId(){
+
+}
+export function listarCarrosDisponiveis(){
+
+}
+export function cadastrarCarro(){
+
+}
+export function atualizarCarro(){
+
+}
+export function deleteCarro(){
+
+}

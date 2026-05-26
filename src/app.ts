5349c4015cb86a4da44151f8406c65c279c103d1
BrunoMendesMorais <mendesdemoraisbruno@gmail.com>
2026-05-26 18:52:30 -0300

    🟢 Feat: Criado rotas para estoque e incrementado algumas funções na controler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import { atualizarCarro, cadastrarCarro, deleteCarro, listaCarros, listarCarroPorId, listarCarrosDisponiveis } from "./controllers/carroController";
+import { atualizarEstoque, cadastrarEstoque, deletaEstoque, listaEstoques, listarEstoquePorId, listarEstoquePorIdCarro } from "./controllers/estoqueController";
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -7,9 +8,18 @@ app.use(express.json());
 
 app.listen(PORT, () => console.log(`API rodando na URL : http://localhost:${PORT}`));
 
+// Carro
 app.get("/carros",listaCarros)
 app.get("/carros/disponiveis",listarCarrosDisponiveis)
 app.get("/carros/:id",listarCarroPorId)
 app.post("/carros",cadastrarCarro)
 app.put("/carros/:id",atualizarCarro)
 app.delete("/carros/:id",deleteCarro)
+
+// Estoque
+app.get("/estoque ",listaEstoques)
+app.get("/estoque/carro/:id_carro",listarEstoquePorIdCarro)
+app.get("/estoque/:id",listarEstoquePorId)
+app.post("/estoque",cadastrarEstoque)
+app.put("/estoque/:id",atualizarEstoque)
+app.delete("/estoque/:id",deletaEstoque)
---
+++ src/controllers/estoqueController.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from "express"
+import { EstoqueService } from "../services/EstoqueService"
+
+const estoqueService = new EstoqueService
+
+export function listaEstoques(req: Request, res: Response){
+    try{
+        res.status(200).json(estoqueService.listaEstoques())
+    }
+    catch(error: any){
+        res.status(404).json({message: error.message})
+    }
+}
+
+export function listarEstoquePorId(req: Request, res: Response){
+    const id:any = req.params.id 
+    try{
+        res.status(200).json(estoqueService.listaEstoquesId(id))
+    }
+    catch(error: any){
+        res.status(404).json({message: error.message})
+    }
+}
+
+export function cadastrarEstoque(req: Request, res: Response){
+    try{
+        const novoEstoque = estoqueService.cadastraEstoque(req.body)
+        res.status(201).json({messagem:"Estoque Criado com sucesso",novoEstoque})
+    }
+    catch(error:any){
+        res.status(400).json(error.message)
+    }
+}
+export function atualizarEstoque(req: Request, res: Response){
+     const EstoqueData = req.body
+     const id = Number(req.params.id)
+    try{
+        const EstoqueAtualizado = estoqueService.atualizaEstoque(EstoqueData,id)
+        res.status(200).json({message:"Estoque atualizado com sucesso",EstoqueAtualizado})
+    }
+    catch(error:any){
+        res.status(400).json(error.message)
+    }
+}
+
+export function listarEstoquePorIdCarro(){
+    
+}
+
+export function deletaEstoque(){
+    
+}

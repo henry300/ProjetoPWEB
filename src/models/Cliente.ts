2f6d7b17809bc714a71a279f614249450cb770e2
henry <webcatcat5@gmail.com>
2026-05-26 16:40:24 -0300

    🔴 Fix: Adicionando telefone ao Cliente.ts

--- src/models/Cliente.ts
+++ src/models/Cliente.ts
@@ -1,13 +1,16 @@
 export class Cliente{
     id_cliente: number
     nome: string
+    telefone: string
     cpf: string
     email: string
     cidade: string
 
-    constructor(nome: string,cpf: string,email: string,cidade: string){
+
+    constructor(nome: string,telefone: string, cpf: string,email: string,cidade: string){
         this.id_cliente = this.geraId()
         this.nome = nome
+        this.telefone = telefone
         this.cpf = cpf
         this.email = email
         this.cidade = cidade

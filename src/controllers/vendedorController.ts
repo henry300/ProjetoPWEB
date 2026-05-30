6bbb827b4f4b513af5e01d5c1e40a169c6dd8fc6
Claudionor-Cosme <claudionorcosmefilho@gmail.com>
2026-05-30 14:40:36 -0300

    🟢 Feat: Adicionado as funções de cadastrar, atualizar e deletar vendedor no Controller

--- src/controllers/vendedorController.ts
+++ src/controllers/vendedorController.ts
@@ -20,6 +20,36 @@ export function listaVendedorPorId(req: Request, res: Response){
         res.status(404).json({message: error.message})
     }
 }
+export function CadastraVendedor(req: Request, res: Response){
+    try{
+        const novoVendedor = vendedorService.CadastraVendedor(req.body)
+        res.status(201).json({message:"Vendedor cadastrado com sucesso",novoVendedor})
+    }
+    catch(error:any){
+        res.status(400).json({message: error.message})
+    }
+}
+export function AtualizaVendedor(req: Request, res: Response){
+    const vendedorData = req.body
+    const id = Number (req.params.id)
+    try{
+        const vendedorAtualizado = vendedorService.AtualizaVendedor(vendedorData,id)
+        res.status(200).json({message:"Vendedor atualizado com sucesso",vendedorAtualizado})
+    }
+    catch(error:any){
+        res.status(400).json({message: error.message})
+    }
+}
+export function deletarVendedor(req: Request, res: Response){
+    const id = Number(req.params.id)
+    try{
+        const resultado = vendedorService.deletarVendedor(id)
+        res.status(200).json({message:"Vendedor deletado com sucessso", resultado})
+    }
+    catch(error:any){
+        res.status(400).json({message: error.message})
+    }
+}
 
 
 

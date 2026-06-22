d6ee74bb4ce37f01573359d8e5a69cd0c504aefb
Claudionor-Cosme <claudionorcosmefilho@gmail.com>
2026-06-22 13:29:55 -0300

    🟢 **Feat: Adicionado controller de vendedor com async e await e tratamento de erros

--- src/controllers/vendedorController.ts
+++ src/controllers/vendedorController.ts
@@ -1,69 +1,68 @@
 import { Request, Response } from "express"
-import { VendedorService } from "../services/VendedorService"
+import { VendedorService } from "../services/VendedorService";
 
-const vendedorService = new VendedorService
+export class VendedorController{
+    private vendedorService = new VendedorService();
 
-export function listaVendedor(req: Request, res: Response) {
+    async listaVendedor(req: Request, res: Response) {
     try {
-        res.status(200).json(vendedorService.listaVendedor())
+        res.status(200).json(await this.vendedorService.listaVendedor())
     }
     catch (error: any) {
-        res.status(error.status).json({ message: error.message })
+        res.status(error.status ?? 500).json({ message: error.message })
     }
 }
 
-export function listaVendedorPorId(req: Request, res: Response) {
+    async listaVendedorPorId(req: Request, res: Response) {
     const id: any = req.params.id
     try {
-        res.status(200).json(vendedorService.listaVendedorPorId(id))
+        res.status(200).json(await this.vendedorService.listaVendedorPorId(id))
     }
     catch (error: any) {
-        res.status(error.status).json({ message: error.message })
+        res.status(error.status ?? 500).json({ message: error.message })
     }
 }
 
-export function CadastraVendedor(req: Request, res: Response) {
+     async CadastraVendedor(req: Request, res: Response) {
     try {
-        const novoVendedor = vendedorService.CadastraVendedor(req.body)
+        const novoVendedor = await this.vendedorService.CadastraVendedor(req.body)
         res.status(201).json({ message: "Vendedor cadastrado com sucesso", novoVendedor })
     }
     catch (error: any) {
-        res.status(error.status).json({ message: error.message })
+        res.status(error.status ?? 500).json({ message: error.message })
     }
 }
 
-export function AtualizaVendedor(req: Request, res: Response) {
+    async AtualizaVendedor(req: Request, res: Response) {
     const vendedorData = req.body
     const id = Number(req.params.id)
     try {
-        const vendedorAtualizado = vendedorService.AtualizaVendedor(vendedorData, id)
+        const vendedorAtualizado = await this.vendedorService.AtualizaVendedor(vendedorData, id)
         res.status(200).json({ message: "Vendedor atualizado com sucesso", vendedorAtualizado })
     }
     catch (error: any) {
-        res.status(error.status).json({ message: error.message })
+        res.status(error.status ?? 500).json({ message: error.message })
     }
 }
 
-export function deletarVendedor(req: Request, res: Response) {
+    async deletarVendedor(req: Request, res: Response) {
     const id = Number(req.params.id)
     try {
-        const resultado = vendedorService.deletarVendedor(id)
+        const resultado = await this.vendedorService.deletarVendedor(id)
         res.status(200).json({ message: "Vendedor deletado com sucessso", resultado })
     }
     catch (error: any) {
-        res.status(error.status).json({ message: error.message })
+        res.status(error.status ?? 500).json({ message: error.message })
     }
 }
 
-export function listaNotasPorVendedor(req: Request, res: Response) {
+    async listaNotasPorVendedor(req: Request, res: Response) {
     const id = Number(req.params.id)
     try {
-        res.status(200).json(vendedorService.listaNotasPorVendedor(id))
+        res.status(200).json(await this.vendedorService.listaNotasPorVendedor(id))
     }
     catch (error: any) {
-        res.status(error.status).json({ message: error.message })
+        res.status(error.status ?? 500).json({ message: error.message })
     }
 }
-
-
-
+} 

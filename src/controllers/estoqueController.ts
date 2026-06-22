46b2c9b37b15383b6dd72b8e73bc25d8ef5726a6
Henry <webcatcat5@gmail.com>
2026-06-22 12:57:54 -0300

    🟢 Feat: Implementando o uso de async em estoqueController.ts

--- src/controllers/estoqueController.ts
+++ src/controllers/estoqueController.ts
@@ -1,67 +1,64 @@
 import { Request, Response } from "express"
 import { EstoqueService } from "../services/EstoqueService"
 
-const estoqueService = new EstoqueService
+export class EstoqueController {
 
-export function listaEstoques(req: Request, res: Response) {
-    try {
-        res.status(200).json(estoqueService.listaEstoques())
-    }
-    catch (error: any) {
-        res.status(error.status).json({ message: error.message })
-    }
-}
+    private estoqueService = new EstoqueService();
 
-export function listarEstoquePorId(req: Request, res: Response) {
-    const id: any = req.params.id
-    try {
-        res.status(200).json(estoqueService.listaEstoquesId(id))
+    async listaEstoque(req: Request, res: Response) {
+        try {
+            return res.status(200).json(await this.estoqueService.listaEstoque())
+        } catch (error: any) {
+            return res.status(error.status).json({ message: error.message })
+        }
     }
-    catch (error: any) {
-        res.status(error.status).json({ message: error.message })
-    }
-}
 
-export function cadastrarEstoque(req: Request, res: Response) {
-    try {
-        const novoEstoque = estoqueService.cadastraEstoque(req.body)
-        res.status(201).json({ message: "Estoque Criado com sucesso", novoEstoque })
+    async listaEstoqueId(req: Request, res: Response) {
+        const id: any = req.params.id
+        try {
+            return res.status(200).json(await this.estoqueService.listaEstoqueId(id))
+        } catch (error: any) {
+            return res.status(error.status).json({ message: error.message })
+        }
     }
-    catch (error: any) {
-        res.status(error.status).json({ message: error.message })
-    }
-}
 
-export function atualizarEstoque(req: Request, res: Response) {
-    const EstoqueData = req.body
-    const id = Number(req.params.id)
-    try {
-        const EstoqueAtualizado = estoqueService.atualizaEstoque(EstoqueData, id)
-        res.status(200).json({ message: "Estoque atualizado com sucesso", EstoqueAtualizado })
+    async cadastraEstoque(req: Request, res: Response) {
+        try {
+            const novoEstoque = await this.estoqueService.cadastraEstoque(req.body)
+            return res.status(201).json({ message: "Estoque Criado com sucesso", novoEstoque })
+        } catch (error: any) {
+            return res.status(error.status).json({ message: error.message })
+        }
     }
-    catch (error: any) {
-        res.status(error.status).json({ message: error.message })
-    }
-}
 
-export function listarEstoquePorIdCarro(req: Request, res: Response) {
-    const id = Number(req.params.id_carro)
-    try {
-        const estoque = estoqueService.listaEstoquesIdCarro(id)
-        res.status(200).json({ quantidade: estoque })
+    async atualizaEstoque(req: Request, res: Response) {
+        const EstoqueData = req.body
+        const id = Number(req.params.id)
+        try {
+            const EstoqueAtualizado = await this.estoqueService.atualizaEstoque(EstoqueData, id)
+            return res.status(200).json({ message: "Estoque atualizado com sucesso", EstoqueAtualizado })
+        } catch (error: any) {
+            return res.status(error.status).json({ message: error.message })
+        }
     }
-    catch (error: any) {
-        res.status(error.status).json({ message: error.message })
-    }
-}
 
-export function deletaEstoque(req: Request, res: Response) {
-    const id = Number(req.params.id)
-    try {
-        const estoque = estoqueService.deletarEstoque(id)
-        res.status(200).json({ message: "Estoque deletado com sucesso", estoque })
+    async listarEstoquePorIdCarro(req: Request, res: Response) {
+        const id = Number(req.params.id_carro)
+        try {
+            const estoque = await this.estoqueService.listaEstoquesIdCarro(id)
+            return res.status(200).json({ quantidade: estoque })
+        } catch (error: any) {
+            return res.status(error.status).json({ message: error.message })
+        }
     }
-    catch (error: any) {
-        res.status(error.status).json({ message: error.message })
+
+    async deletaEstoque(req: Request, res: Response) {
+        const id = Number(req.params.id)
+        try {
+            const estoque = await this.estoqueService.deletarEstoque(id)
+            return res.status(200).json({ message: "Estoque deletado com sucesso", estoque })
+        } catch (error: any) {
+            return res.status(error.status).json({ message: error.message })
+        }
     }
 }

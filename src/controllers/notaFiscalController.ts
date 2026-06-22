3ed5f09abe6cdd12620ce68e910c3effacc48955
Henry <webcatcat5@gmail.com>
2026-06-22 13:33:30 -0300

    🟢 Feat: Implementando o uso de async em notaFiscalController.ts

--- src/controllers/notaFiscalController.ts
+++ src/controllers/notaFiscalController.ts
@@ -1,33 +1,33 @@
 import { Request, Response } from "express"
 import { NotaFiscalService } from "../services/NotaFiscalService"
 
-const notaFiscalService = new NotaFiscalService
+export class NotaFiscalController {
+    
+    private notaFiscalService = new NotaFiscalService()
 
-export function listarNotaFiscal(req: Request, res: Response) {
-    try {
-        res.status(200).json(notaFiscalService.listaNotas())
+    async listarNotaFiscal(req: Request, res: Response) {
+        try {
+            return res.status(200).json(await this.notaFiscalService.listaNotas())
+        } catch (error: any) {
+            return res.status(error.status).json({ message: error.message })
+        }
     }
-    catch (error: any) {
-        res.status(error.status).json({ message: error.message })
-    }
-}
 
-export function listarNotaFiscalPorId(req: Request, res: Response) {
-    const id: number = Number(req.params.id)
-    try {
-        res.status(200).json(notaFiscalService.listaNotaPorId(id))
+    async listarNotaFiscalPorId(req: Request, res: Response) {
+        const id: number = Number(req.params.id)
+        try {
+            return res.status(200).json(await this.notaFiscalService.listaNotaPorId(id))
+        } catch (error: any) {
+            return res.status(error.status).json({ message: error.message })
+        }
     }
-    catch (error: any) {
-        res.status(error.status).json({ message: error.message })
-    }
-}
 
-export function criarNotaFiscal(req: Request, res: Response) {
-    const notaData = req.body
-    try {
-        res.status(201).json(notaFiscalService.emiteNota(notaData))
-    }
-    catch (error: any) {
-        res.status(error.status).json({ message: error.message })
+    async criarNotaFiscal(req: Request, res: Response) {
+        const notaData = req.body
+        try {
+            return res.status(201).json(await this.notaFiscalService.emiteNota(notaData))
+        } catch (error: any) {
+            return res.status(error.status).json({ message: error.message })
+        }
     }
 }

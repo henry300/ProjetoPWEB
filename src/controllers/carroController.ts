e8b3bbe9cd20729a02a5c1c9dece6dec7f3798db
Claudionor-Cosme <claudionorcosmefilho@gmail.com>
2026-06-22 11:41:32 -0300

    🟢 **Feat: implementado controller de carros com async e await e tratamento de erros

--- src/controllers/carroController.ts
+++ src/controllers/carroController.ts
@@ -3,61 +3,61 @@ import { CarroService } from "../services/CarroService"
 
 const carroService = new CarroService
 
-export function listaCarros(req: Request, res: Response) {
+export async function listaCarros(req: Request, res: Response) {
     try {
-        res.status(200).json(carroService.listaCarros())
+        res.status(200).json(await carroService.listaCarros())
     }
     catch (error: any) {
-        res.status(error.status).json({ message: error.message })
+        res.status(error.status ?? 500).json({ message: error.message })
     }
 }
 
-export function listarCarroPorId(req: Request, res: Response) {
+export async function listarCarroPorId(req: Request, res: Response) {
     const id: any = req.params.id
     try {
-        res.status(200).json(carroService.listaCarrosId(id))
+        res.status(200).json(await carroService.listaCarrosId(id))
     }
     catch (error: any) {
-        res.status(error.status).json({ message: error.message })
+        res.status(error.status ?? 500).json({ message: error.message })
     }
 }
 
-export function listarCarrosDisponiveis(req: Request, res: Response) {
+export async function listarCarrosDisponiveis(req: Request, res: Response) {
     try {
-        res.status(200).json(carroService.listaCarrosDisponiveis())
+        res.status(200).json(await carroService.listaCarrosDisponiveis())
     }
     catch (error: any) {
-        res.status(error.status).json({ message: error.message })
+        res.status(error.status ?? 500).json({ message: error.message })
     }
 }
 
-export function cadastrarCarro(req: Request, res: Response) {
+export async function cadastrarCarro(req: Request, res: Response) {
     try {
-        const novoCarro = carroService.cadastraCarro(req.body)
+        const novoCarro = await carroService.cadastraCarro(req.body)
         res.status(201).json({ message: "Carro criado com sucesso", novoCarro })
     }
     catch (error: any) {
-        res.status(error.status).json({ message: error.message })
+        res.status(error.status ?? 500).json({ message: error.message })
     }
 }
-export function atualizarCarro(req: Request, res: Response) {
+export async function atualizarCarro(req: Request, res: Response) {
     const carroData = req.body
     const id = Number(req.params.id)
     try {
-        const carroAtualizado = carroService.atualizaCarro(carroData, id)
+        const carroAtualizado = await carroService.atualizaCarro(carroData, id)
         res.status(200).json({ message: "Carro atualizado com sucesso", carroAtualizado })
     }
     catch (error: any) {
-        res.status(error.status).json({ message: error.message })
+        res.status(error.status ?? 500).json({ message: error.message })
     }
 }
-export function deleteCarro(req: Request, res: Response) {
+export async function deleteCarro(req: Request, res: Response) {
     const id = Number(req.params.id)
     try {
-        const carroDeletado = carroService.deletaCarro(id)
+        const carroDeletado = await carroService.deletaCarro(id)
         res.status(200).json({ message: "Carro deletado com sucesso", carroDeletado })
     }
     catch (error: any) {
-        res.status(error.status).json({ message: error.message })
+        res.status(error.status ?? 500).json({ message: error.message })
     }
 }
